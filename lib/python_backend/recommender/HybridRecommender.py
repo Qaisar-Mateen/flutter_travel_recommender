288fde270b6e8e0c69320e8f8121723d21135b5e
Qaisar-Mateen <qaisarmateen313@gmail.com>
2024-08-02 00:21:24 +0500

    added the Login API and it's used files

--- lib/python_backend/recommender/HybridRecommender.py
+++ lib/python_backend/recommender/HybridRecommender.py
@@ -79,8 +79,8 @@ if __name__ == '__main__':
     print()
 
     hr2 = HybridRecommender(collaborative_model=(True, 0, 'CF_Neural_Model3.7.bin'),
-                        popularity_model=True, content_model=True,
-                        popular_weight=0.15, collab_weight=0.7, content_weight=0.15
-                        )
+                popularity_model=True, content_model=True,
+                popular_weight=0.15, collab_weight=0.7, content_weight=0.15
+            )
     
     print(hr2.recommend(top_n=16))
--- lib/python_backend/serverAPI.py
+++ lib/python_backend/serverAPI.py
@@ -1,8 +1,22 @@
-import recommender.HybridRecommender as hr
 import flask
-#from flask import jsonify
+import pandas as pd
+from flask import jsonify, request
+import recommender.HybridRecommender as hr
 
-app = flask.Flask(__name__) 
+app = flask.Flask(__name__)
+
+@app.route("/login", methods=["GET"])
+def login():
+    userId = request.args.get("userId")
+    ids = pd.read_csv('recommender/ratings.csv')
+    ids = ids['user'].unique()
+
+    if userId in ids:
+        return True
+    return False
+
+
+
 
 if __name__ == "__main__":
     app.run(debug= True, host="0.0.0.0", port=5000)
